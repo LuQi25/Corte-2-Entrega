25afe32d5694c13851b2b2215826a73e72094b67
LuQi25 <luis17490@gmail.com>
2021-10-22 20:46:27 -0500

    commit 1

--- Assets/scripts/Bullet.cs
+++ Assets/scripts/Bullet.cs
@@ -12,43 +12,45 @@ public class Bullet : MonoBehaviour
 
     public float coolddown = 2; //cooldown para ritmo que salen las balas cada cierto tiempo
     private float nextTime;
+    
 
     private void Start()
     {
         myAnimator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
 
     
     void Update()
     {
-        transform.Translate(Vector3.right * Bspeed * Time.deltaTime);
+        transform.Translate(Vector3.right * Bspeed * Time.deltaTime); //velocidad de la bala
+        
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
 
-        Bspeed = 0;
-        Destroy(gameObject);
-
-
-        if (collision.gameObject.layer == 3)
-        {
-            //nextTime = Time.time + coolddown;
-           // myAnimator.SetBool("BulletOff", true);
-            //if (Time.time > nextTime)
-            //{
-
-                //Destroy(gameObject);
-
-            //}
-        }
-        else
-        {
-            //myAnimator.SetBool("BulletOff", false);
-        }
         
 
+
+        if (collision.gameObject.layer == 3) // si choca con la capa del suelo se hace la animacion y luego se comienza el ienumerator.
+        {
+            transform.GetComponent<Animator>().SetBool("BulletOff", true);
+            StartCoroutine(DestroyBullet());
+            
+
+        }
+      
+        
+
+    }
+    IEnumerator DestroyBullet() //se usa para ejecutar en su totalidad la animacion de explosion.
+    {
+        Bspeed = 0;
+        rb.velocity = new Vector2(0, rb.velocity.y);
+        yield return new WaitForSeconds(0.2f);
+        Destroy(gameObject);
     }
 
 
